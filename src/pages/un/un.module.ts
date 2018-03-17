873a2da4a6643cab83bdc4dc3df8cc2e6a7886f6
Med1470 <mohamedbejaoui1470@gmail.com>
2018-03-17 12:22:46 +0100

    first

---
+++ src/pages/un/un.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UnPage } from './un';
+
+@NgModule({
+  declarations: [
+    UnPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UnPage),
+  ],
+})
+export class UnPageModule {}
